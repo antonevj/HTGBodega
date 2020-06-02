4e3e15dd47be5d47e9853fcb8b522f14bcb53f0c
antonevj <antonevj26@gmail.com>
2020-06-02 11:45:09 -0500

    Creacion de POO

---
+++ HTGBodega.Contrac/Base/IRead.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTGBodega.Contrac.Base
+{
+   public interface IRead<T,Y> where T : class
+    {
+        //obtener todo  los registros  de la base de datos 
+
+        IEnumerable<T> GetAll();
+
+        //obtener un registro especifico
+
+        T Get(Y id);
+    }
+
+}
